d2373fc8ad0ce333e2edd251c5f934c41b192d2a
lezav22 <lezav22@gmail.com>
2023-11-16 17:18:52 -0500

    inicia integración/consumo de apiRest para traer productos dinamicamente

--- main.js
+++ main.js
@@ -10,7 +10,26 @@ menuCuenta.addEventListener("click", function(){
 const lineMenu = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
 lineMenu.addEventListener("click", function (){
+    //inactivamos el carrito para evitar superposicion
+    divCarrito.classList.add('inactive');
+    //activamos el menuMobile
     menuMobile.classList.toggle('inactive');
 });
 
 
+// mostrando y ocultando carrito de compras
+
+const liCarrito = document.querySelector('.navbar-shopping-cart');
+const divCarrito = document.querySelector('.product-detail');
+liCarrito.addEventListener("click",function(){
+    //inactivamos menuMobile para evitar superposicion
+    menuMobile.classList.add('inactive');
+    menuDesktop.classList.add('inactive');
+    //Activamos el carrito
+    divCarrito.classList.toggle('inactive');
+});
+
+
+//RAPID API - ENCUENTRO CIENTOS DE APIS GRATIS https://rapidapi.com/
+//API PLATZI PRODUCTS
+//https://api.escuelajs.co/api/v1/products
